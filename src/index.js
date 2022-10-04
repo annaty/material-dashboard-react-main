2419baa0603080e0e94b5076bdb26e93e29adecd
annaty <tylkowskanna@gmail.com>
2022-10-04 11:28:19 +0200

    changed app so that it shows homepage by default

--- src/index.js
+++ src/index.js
@@ -18,14 +18,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
-// Material Dashboard 2 React Context Provider
-import { MaterialUIControllerProvider } from "context";
-
 ReactDOM.render(
   <BrowserRouter>
-    <MaterialUIControllerProvider>
-      <App />
-    </MaterialUIControllerProvider>
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
