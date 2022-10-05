c769aee9ca3243db10d4106e222bec7d155d2272
annaty <tylkowskanna@gmail.com>
2022-10-05 16:06:55 +0200

    fixed login

--- client/src/layouts/authentication/Login/Login.js
+++ client/src/layouts/authentication/Login/Login.js
@@ -1,32 +1,70 @@
-import React from 'react';
+/* eslint-disable react/no-unused-class-component-methods */
+import React from "react";
 
 class Login extends React.Component {
-    state={
-    email: '',
-    psw: ''
-    }
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      psw: "",
+    };
 
-    handleOnChange = (e) => {
-        const {name, value} = e.target;
-    }
+    this.request = new Request("http://localhost:5000/api", {
+      method: "post",
+      body: JSON.stringify(this.state),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    });
 
-    handleOnSubmit = (e) => {
-        const {email, psw} = this.state;
-    }
+    this.handleOnChangeEmail = (e) => {
+      this.email = e;
+    };
 
+    this.handleOnChangePassword = (e) => {
+      this.pw = e;
+    };
 
-    render() {
-        return (
-            <div>
-                <h1>Login</h1>
-                <form onSubmit= {this.onSubmit}>
-                    <input type="email" name='email' placeholder='email...' required onChange={this.onChange} />
-                    <input type="password" name='psw' placeholder='password...' required onChange= {this.onChange} />
-                    <button onSubmit= {this.onSubmit}>Log In</button>
-                </form>
-            </div>
-        );
-    }
+    this.handleOnSubmit = (e) => {
+      fetch(this.request)
+        .then((res) => {
+          if (res.status === 200) {
+            console.log(e);
+            return res.json();
+          }
+          return "hello";
+        })
+        .catch((error) => error);
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleOnSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="email..."
+            required
+            onChange={this.handleOnChangeEmail}
+          />
+          <input
+            type="password"
+            name="psw"
+            placeholder="password..."
+            required
+            onChange={this.handleOnChangePassword}
+          />
+          <button type="submit" onSubmit={this.handleOnSubmit}>
+            Log In
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
