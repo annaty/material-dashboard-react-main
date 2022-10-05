0eae993316f3ec08881ed89f8bc9014a1683adf8
thomasBarille-ab <thomasbarille@gmail.com>
2022-10-05 10:50:46 +0200

    init bdd

--- server/server.js
+++ server/server.js
@@ -5,4 +5,20 @@ app.get("/api", (req, res) => {
     res.json({users: ["userOne", "userTwo", "userThree", "userFour"]})
 })
 
-app.listen(5000, () => {console.log("Server is running on port 5000")})
+app.listen(5000, () => { console.log("Server is running on port 5000") })
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'dbuser',
+    password: 's3kreee7'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
